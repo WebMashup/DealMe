131c77fa1f978bd82e03d5bfdf9ce0db6309c962
nrahnemoon <nrahnemoon@gmail.com>
2012-05-15 14:36:07 -0700

    Removed a bunch of files that we weren't using.  Use index.html only from now on.

--- src/me/deal/client/events/UserLocationEventHandler.java
+++
@@ -1,7 +0,0 @@
-package me.deal.client.events;
-
-import com.google.gwt.event.shared.EventHandler;
-
-public interface UserLocationEventHandler extends EventHandler {
-  void onUserLocation(UserLocationEvent event);
-}
